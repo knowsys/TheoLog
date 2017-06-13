4ad27b1d232ba35423b535de3e7d001d8f4f3241
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2017-06-13 21:00:16 +0200

    Use LuaLaTeX
    Use LuaLaTeX instead of pdfLaTeX to build the slides.  This fixes #5.
    Use latexmk instead of relying on two runs sufficing to build the documents.

--- Vorlesungen/build.sh
+++ Vorlesungen/build.sh
@@ -5,23 +5,23 @@
 # Usage sh build.sh <lectureNumberAsString>
 
 fileprefix="TheoLog2017-Vorlesung-" # the start of generated PDFs
-tmpfilename="tmplecturefile" # don't have any files called like this!
+tmpfilename="`mktemp tmplecturefile-XXX`" # generate a name that doesn't collide with anything present
 
 printfilename=$fileprefix$1-print.pdf
 overlayfilename=$fileprefix$1-overlay.pdf
 
 printf "\\documentclass[aspectratio=1610,onlymath,handout]{beamer}\n\n" > $tmpfilename.tex
 tail -n +3 lecture-$1.tex >> $tmpfilename.tex
-pdflatex $tmpfilename.tex
-pdflatex $tmpfilename.tex
+latexmk -lualatex ${tmpfilename}.tex
+latexmk -c ${tmpfilename}.tex
 pdfnup --nup 2x2 --outfile $printfilename $tmpfilename.pdf
 
 printf "\\documentclass[aspectratio=1610,onlymath]{beamer}\n\n" > $tmpfilename.tex
 tail -n +3 lecture-$1.tex >> $tmpfilename.tex
-pdflatex $tmpfilename.tex
-pdflatex $tmpfilename.tex
+latexmk -lualatex ${tmpfilename}.tex
+latexmk -c ${tmpfilename}.tex
 mv $tmpfilename.pdf $overlayfilename
 
-rm $tmpfilename.*
+rm $tmpfilename $tmpfilename}.*
 
 printf "\n\n*** Finished creating files '$overlayfilename' and '$printfilename' ***\n"
