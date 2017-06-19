21d2058bd47c6d7815b1e308fec06df691c4da66
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2017-06-19 22:43:28 +0200

    Use consistent notation for variables

--- Vorlesungen/build.sh
+++ Vorlesungen/build.sh
@@ -7,21 +7,25 @@
 fileprefix="TheoLog2017-Vorlesung-" # the start of generated PDFs
 tmpfilename="`mktemp tmplecturefile-XXX`" # generate a name that doesn't collide with anything present
 
-printfilename=$fileprefix$1-print.pdf
-overlayfilename=$fileprefix$1-overlay.pdf
+printfilename=${fileprefix}${1}-print.pdf
+overlayfilename=${fileprefix}${1}-overlay.pdf
 
-printf "\\documentclass[aspectratio=1610,onlymath,handout]{beamer}\n\n" > $tmpfilename.tex
-tail -n +3 lecture-$1.tex >> $tmpfilename.tex
+echo '\documentclass[onlymath,handout]{beamer}' > ${tmpfilename}.tex
+tail -n +3 lecture-${1}.tex >> ${tmpfilename}.tex
 latexmk -lualatex ${tmpfilename}.tex
 latexmk -c ${tmpfilename}.tex
-pdfnup --nup 2x2 --outfile $printfilename $tmpfilename.pdf
+pdfnup --nup 2x2 --outfile ${printfilename} ${tmpfilename}.pdf
 
-printf "\\documentclass[aspectratio=1610,onlymath]{beamer}\n\n" > $tmpfilename.tex
-tail -n +3 lecture-$1.tex >> $tmpfilename.tex
+echo '\documentclass[onlymath]{beamer}' > ${tmpfilename}.tex
+tail -n +3 lecture-${1}.tex >> ${tmpfilename}.tex
+
 latexmk -lualatex ${tmpfilename}.tex
 latexmk -c ${tmpfilename}.tex
-mv $tmpfilename.pdf $overlayfilename
+mv ${tmpfilename}.pdf ${overlayfilename}
 
 rm ${tmpfilename} ${tmpfilename}.*
 
-printf "\n\n*** Finished creating files '$overlayfilename' and '$printfilename' ***\n"
+echo
+echo
+echo "*** Finished creating files '${overlayfilename}' and '${printfilename}' ***"
+echo
