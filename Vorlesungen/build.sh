fbbe22745afd43df8594b7ee0eee861c23e8d747
mkroetzsch <accounts@korrekt.org>
2018-04-08 23:16:53 +0200

    it's 2018

--- Vorlesungen/build.sh
+++ Vorlesungen/build.sh
@@ -4,7 +4,7 @@
 #
 # Usage sh build.sh <lectureNumberAsString>
 
-fileprefix="TheoLog2017-Vorlesung-" # the start of generated PDFs
+fileprefix="TheoLog2018-Vorlesung-" # the start of generated PDFs
 tmpfilename="tmplecturefile" # don't have any files called like this!
 
 printfilename=$fileprefix$1-print.pdf
