24239f82d23a7f421948624d8503887ed4c24339
mkroetzsch <accounts@korrekt.org>
2021-04-15 11:27:33 +0200

    diable print build

--- Vorlesungen/build.sh
+++ Vorlesungen/build.sh
@@ -4,17 +4,17 @@
 #
 # Usage sh build.sh <lectureNumberAsString>
 
-fileprefix="TheoLog2018-Vorlesung-" # the start of generated PDFs
+fileprefix="TheoLog2021-Vorlesung-" # the start of generated PDFs
 tmpfilename="tmplecturefile" # don't have any files called like this!
 
 printfilename=$fileprefix$1-print.pdf
 overlayfilename=$fileprefix$1-overlay.pdf
 
-printf "\\documentclass[aspectratio=1610,onlymath,handout]{beamer}\n\n" > $tmpfilename.tex
-tail -n +3 lecture-$1.tex >> $tmpfilename.tex
-pdflatex $tmpfilename.tex
-pdflatex $tmpfilename.tex
-pdfnup --nup 2x2 --outfile $printfilename $tmpfilename.pdf
+# printf "\\documentclass[aspectratio=1610,onlymath,handout]{beamer}\n\n" > $tmpfilename.tex
+# tail -n +3 lecture-$1.tex >> $tmpfilename.tex
+# pdflatex $tmpfilename.tex
+# pdflatex $tmpfilename.tex
+# pdfnup --nup 2x2 --outfile $printfilename $tmpfilename.pdf
 
 printf "\\documentclass[aspectratio=1610,onlymath]{beamer}\n\n" > $tmpfilename.tex
 tail -n +3 lecture-$1.tex >> $tmpfilename.tex
