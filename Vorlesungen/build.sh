5c1a3c15415c61fc3bb3ed48295d99d4dba6147a
Markus Krötzsch <markus.kroetzsch@tu-dresden.de>
2024-04-08 16:48:43 +0200

    update year to 2024

--- Vorlesungen/build.sh
+++ Vorlesungen/build.sh
@@ -4,16 +4,16 @@
 #
 # Usage sh build.sh <lectureNumberAsString>
 
-fileprefix="TheoLog2021-Vorlesung-" # the start of generated PDFs
+fileprefix="TheoLog2024-Vorlesung-" # the start of generated PDFs
 tmpfilename="tmplecturefile" # don't have any files called like this!
 
 printfilename=$fileprefix$1-print.pdf
 overlayfilename=$fileprefix$1-overlay.pdf
 
-# printf "\\documentclass[aspectratio=1610,onlymath,handout]{beamer}\n\n" > $tmpfilename.tex
-# tail -n +3 lecture-$1.tex >> $tmpfilename.tex
-# pdflatex $tmpfilename.tex
-# pdflatex $tmpfilename.tex
+printf "\\documentclass[aspectratio=1610,onlymath,handout]{beamer}\n\n" > $tmpfilename.tex
+tail -n +3 lecture-$1.tex >> $tmpfilename.tex
+pdflatex $tmpfilename.tex
+pdflatex $tmpfilename.tex
 # pdfnup --nup 2x2 --outfile $printfilename $tmpfilename.pdf
 
 printf "\\documentclass[aspectratio=1610,onlymath]{beamer}\n\n" > $tmpfilename.tex
